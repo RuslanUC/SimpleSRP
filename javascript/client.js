ea523a88fda53ab5c2206ce6f5432b8e597abeec
RuslanUC <dev_ruslan_uc@protonmail.com>
2023-04-18 18:15:58 +0300

    add client js script

---
+++ javascript/client.js
@@ -0,0 +1,294 @@
+NG_VALUES = {
+    1024: {
+        "N": "EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496" +
+            "EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8E" +
+            "F4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA" +
+            "9AFD5138FE8376435B9FC61D2FC0EB06E3",
+        "g": "02"
+    },
+    2048: {
+        "N": "AC6BDB41324A9A9BF166DE5E1389582FAF72B6651987EE07FC3192943DB56050A37329CBB4" +
+            "A099ED8193E0757767A13DD52312AB4B03310DCD7F48A9DA04FD50E8083969EDB767B0CF60" +
+            "95179A163AB3661A05FBD5FAAAE82918A9962F0B93B855F97993EC975EEAA80D740ADBF4FF" +
+            "747359D041D5C33EA71D281E446B14773BCA97B43A23FB801676BD207A436C6481F1D2B907" +
+            "8717461A5B9D32E688F87748544523B524B0D57D5EA77A2775D2ECFA032CFBDBF52FB37861" +
+            "60279004E57AE6AF874E7303CE53299CCC041C7BC308D82A5698F3A8D0C38271AE35F8E9DB" +
+            "FBB694B5C803D89F7AE435DE236D525F54759B65E372FCD68EF20FA7111F9E4AFF73",
+        "g": "02"
+    },
+    4096: {
+        "N": "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E08" +
+            "8A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B" +
+            "302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9" +
+            "A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE6" +
+            "49286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8" +
+            "FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
+            "670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C" +
+            "180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF695581718" +
+            "3995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D" +
+            "04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7D" +
+            "B3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D226" +
+            "1AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200C" +
+            "BBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFC" +
+            "E0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B26" +
+            "99C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB" +
+            "04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2" +
+            "233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127" +
+            "D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C934063199" +
+            "FFFFFFFFFFFFFFFF",
+        "g": "05"
+    },
+    8192: {
+        "N": "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E08" +
+            "8A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B" +
+            "302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9" +
+            "A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE6" +
+            "49286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8" +
+            "FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
+            "670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C" +
+            "180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF695581718" +
+            "3995497CEA956AE515D2261898FA051015728E5A8AAAC42DAD33170D" +
+            "04507A33A85521ABDF1CBA64ECFB850458DBEF0A8AEA71575D060C7D" +
+            "B3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D226" +
+            "1AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200C" +
+            "BBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFC" +
+            "E0FD108E4B82D120A92108011A723C12A787E6D788719A10BDBA5B26" +
+            "99C327186AF4E23C1A946834B6150BDA2583E9CA2AD44CE8DBBBC2DB" +
+            "04DE8EF92E8EFC141FBECAA6287C59474E6BC05D99B2964FA090C3A2" +
+            "233BA186515BE7ED1F612970CEE2D7AFB81BDD762170481CD0069127" +
+            "D5B05AA993B4EA988D8FDDC186FFB7DC90A6C08F4DF435C934028492" +
+            "36C3FAB4D27C7026C1D4DCB2602646DEC9751E763DBA37BDF8FF9406" +
+            "AD9E530EE5DB382F413001AEB06A53ED9027D831179727B0865A8918" +
+            "DA3EDBEBCF9B14ED44CE6CBACED4BB1BDB7F1447E6CC254B33205151" +
+            "2BD7AF426FB8F401378CD2BF5983CA01C64B92ECF032EA15D1721D03" +
+            "F482D7CE6E74FEF6D55E702F46980C82B5A84031900B1C9E59E7C97F" +
+            "BEC7E8F323A97A7E36CC88BE0F1D45B7FF585AC54BD407B22B4154AA" +
+            "CC8F6D7EBF48E1D814CC5ED20F8037E0A79715EEF29BE32806A1D58B" +
+            "B7C5DA76F550AA3D8A1FBFF0EB19CCB1A313D55CDA56C9EC2EF29632" +
+            "387FE8D76E3C0468043E8F663F4860EE12BF2D5B0B7474D6E694F91E" +
+            "6DBE115974A3926F12FEE5E438777CB6A932DF8CD8BEC4D073B931BA" +
+            "3BC832B68D9DD300741FA7BF8AFC47ED2576F6936BA424663AAB639C" +
+            "5AE4F5683423B4742BF1C978238F16CBE39D652DE3FDB8BEFC848AD9" +
+            "22222E04A4037C0713EB57A81A23F0C73473FC646CEA306B4BCBC886" +
+            "2F8385DDFA9D4B7FA2C087E879683303ED5BDD3A062B3CF5B3A278A6" +
+            "6D2A13F83F44F82DDF310EE074AB6A364597E899A0255DC164F31CC5" +
+            "0846851DF9AB48195DED7EA1B1D510BD7EE74D73FAF36BC31ECFA268" +
+            "359046F4EB879F924009438B481C6CD7889A002ED5EE382BC9190DA6" +
+            "FC026E479558E4475677E9AA9E3050E2765694DFC81F56E880B96E71" +
+            "60C980DD98EDD3DFFFFFFFFFFFFFFFFF",
+        "g": "16"
+    },
+}
+
+const getRanHex = (size_bytes) => {
+    let result = "";
+    let hexRef = "0123456789abcdef";
+
+    for (let n = 0; n < size_bytes*2; n++) {
+        result += hexRef[(Math.random() * 16) >> 0];
+    }
+    return result;
+}
+
+function concatUint8Arrays(arrays) {
+    // sum of individual array lengths
+    let totalLength = arrays.reduce((acc, value) => acc + value.length, 0);
+    let result = new Uint8Array(totalLength);
+    let length = 0;
+    for(let array of arrays) {
+        result.set(array, length);
+        length += array.length;
+    }
+
+    return result;
+}
+
+class SrpBigInteger {
+    constructor(value) {
+        if(value instanceof Uint8Array)
+            value = value.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+        this._length = value.length;
+        this._bigInt = BigInt("0x"+value);
+    }
+
+    _copy() {
+        let bi = new SrpBigInteger("00");
+        bi._bigInt = this._bigInt;
+        return bi;
+    }
+
+    _getBigInt(val) {
+        if(val instanceof SrpBigInteger)
+            return val._bigInt;
+        if(typeof(val) !== "bigint")
+            return BigInt(val);
+        return val;
+    }
+
+    // https://stackoverflow.com/questions/5989429/pow-and-mod-function-optimization
+    _expmod(base, exponent, modulus){
+        if (exponent === 0n) return 1n;
+        if (exponent % 2n === 0n) {
+            return (this._expmod(base, (exponent / 2n), modulus) ** 2n) % modulus;
+        } else {
+            return (base * this._expmod(base, (exponent - 1n), modulus)) % modulus;
+        }
+    }
+
+    modPow(exponent, modulus) {
+        exponent = this._getBigInt(exponent);
+        modulus = this._getBigInt(modulus);
+        let newBigInt = this._copy();
+        newBigInt._bigInt = this._expmod(newBigInt._bigInt, exponent, modulus);
+        return newBigInt;
+    }
+
+    toString(radix) {
+        return this._bigInt.toString(radix);
+    }
+
+    toStringHex() {
+        let hex = this._bigInt.toString(16);
+        if(hex.length % 2 !== 0) hex = "0"+hex;
+        hex = hex.padStart(this._length, "0");
+        return hex;
+    }
+
+    bitLength() {
+        return this._bigInt.toString(2).length;
+    }
+
+    mod(modulus) {
+        modulus = this._getBigInt(modulus);
+        let newBigInt = this._copy();
+        newBigInt._bigInt = newBigInt._bigInt % modulus;
+        return newBigInt;
+    }
+
+    multiply(multiplier) {
+        multiplier = this._getBigInt(multiplier);
+        let newBigInt = this._copy();
+        newBigInt._bigInt = newBigInt._bigInt * multiplier;
+        return newBigInt;
+    }
+
+    subtract(int) {
+        int = this._getBigInt(int);
+        let newBigInt = this._copy();
+        newBigInt._bigInt = newBigInt._bigInt - int;
+        return newBigInt;
+    }
+
+    add(int) {
+        int = this._getBigInt(int);
+        let newBigInt = this._copy();
+        newBigInt._bigInt = newBigInt._bigInt + int;
+        return newBigInt;
+    }
+
+    compareTo(other) {
+        other = this._getBigInt(other);
+        let newBigInt = this._copy();
+        newBigInt._bigInt = other - newBigInt._bigInt;
+        return newBigInt;
+    }
+
+    toUint8Array() {
+        return Uint8Array.from(this.toStringHex().match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+    }
+}
+
+class SrpClient {
+    constructor(username, password, hash_alg, ng) {
+        this.username = username;
+        this.password = password;
+        this.hash_alg = hash_alg;
+
+        this.N = new SrpBigInteger(NG_VALUES[ng]["N"]);
+        this.g = new SrpBigInteger(NG_VALUES[ng]["g"]);
+    }
+
+    async hashBytes(...bytearrs) {
+        let bytearr = concatUint8Arrays(bytearrs);
+        const hashBuffer = await crypto.subtle.digest(this.hash_alg.toUpperCase(), bytearr);
+        return new SrpBigInteger(new Uint8Array(hashBuffer));
+    }
+
+    async hashString(str) {
+        return await this.hashBytes(new TextEncoder().encode(str));
+    }
+
+    async preCalculateK() {
+        this.k = await this.hashBytes(this.N.toUint8Array(), this.g.toUint8Array());
+        return this.k;
+    }
+
+    genSalt() {
+        this.salt = new SrpBigInteger(getRanHex(32));
+        return this.salt;
+    }
+
+    setSalt(salt) {
+        this.salt = salt;
+    }
+
+    async genX() {
+        this.x = await this.hashBytes(this.salt.toUint8Array(), (await this.hashString(`${this.username}:${this.password}`)).toUint8Array());
+        return this.x;
+    }
+
+    async genV() {
+        this.V = this.g.modPow(await this.genX(), this.N);
+        return this.V;
+    }
+
+    async genA() {
+        this.a = (new SrpBigInteger(getRanHex(128))).mod(this.N);
+        if(this.a.mod(this.N)._bigInt === 0n)
+            this.a = this.N.subtract(1);
+        this.A = this.g.modPow(this.a, this.N);
+        return this.A;
+    }
+
+    async calcM() {
+        let Hg = await this.hashBytes(this.g.toUint8Array());
+        let HN = await this.hashBytes(this.N.toUint8Array());
+        let HI = await this.hashString(this.username);
+        Hg._length = 64;
+        HN._length = 64;
+        let HgU = Hg.toUint8Array();
+        let HNU = HN.toUint8Array();
+        let HxorU = new Uint8Array(HgU.length);
+        for(let i = 0; i < HgU.length; i++) {
+            HxorU[i] = HgU[i] ^ HNU[i];
+        }
+        let Hxor = new SrpBigInteger(HxorU);
+        this.M = await this.hashBytes(
+            Hxor.toUint8Array(),
+            HI.toUint8Array(),
+            this.salt.toUint8Array(),
+            this.A.toUint8Array(),
+            this.B.toUint8Array(),
+            this.K.toUint8Array()
+        );
+        return this.M;
+    }
+
+    setB(B) {
+        this.B = B;
+    }
+
+    async processChallenge() {
+        let u = await this.hashBytes(this.A.toUint8Array(), this.B.toUint8Array());
+        let x = await this.genX();
+        this.S = this.B.subtract(this.k.multiply(this.g.modPow(x, this.N))).modPow(this.a.add(u.multiply(x)), this.N);
+        this.K = await this.hashBytes(this.S.toUint8Array());
+        await this.calcM();
+        this.H_AMK = await this.hashBytes(this.A.toUint8Array(), this.M.toUint8Array(), this.K.toUint8Array());
+        return this.M;
+    }
+
+    verify_HAMK(server_HAMK) {
+        return server_HAMK.compareTo(this.H_AMK) === 0;
+    }
+}
